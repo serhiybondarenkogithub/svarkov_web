82c3c4f7030868196d1e853a123ba23e1f7d8e3a
Serhiy Bondarenko <sergiogetstudy@gmail.com>
2024-02-01 15:30:26 +0200

    fixed port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const path = require('path');
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8089;
 
 const express = require('express');
 const bodyParser = require('body-parser');
